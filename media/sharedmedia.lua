37ea7b4363355767121888b7587c71c1f5b9e75a
Caliburnus <zoe.testament@gmail.com>
2012-01-19 23:19:26 -0600

    Initial commit

---
+++ media/sharedmedia.lua
@@ -0,0 +1,17 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+if LSM == nil then return end
+
+LSM:Register("statusbar","ElvUI Gloss", [[Interface\AddOns\ElvUI\media\textures\normTex.tga]])
+LSM:Register("statusbar","ElvUI Norm", [[Interface\AddOns\ElvUI\media\textures\normTex2.tga]])
+LSM:Register("statusbar","Asphyxia Gloss", [[Interface\AddOns\ElvUI\media\textures\asphyxia.tga]])
+LSM:Register("background","ElvUI Blank", [[Interface\BUTTONS\WHITE8X8]])
+LSM:Register("border", "ElvUI GlowBorder", [[Interface\AddOns\ElvUI\media\textures\glowTex.tga]])
+LSM:Register("font","ElvUI Font", [[Interface\AddOns\ElvUI\media\fonts\expressway.ttf]])
+LSM:Register("font", "ElvUI Alt-Font", [[Interface\AddOns\ElvUI\media\fonts\Continuum_Medium.ttf]])
+LSM:Register("font", "ElvUI Pixel", [[Interface\AddOns\ElvUI\media\fonts\Homespun.ttf]])
+LSM:Register("font","ElvUI Combat", [[Interface\AddOns\ElvUI\media\fonts\Action_Man.ttf]])
+LSM:Register("font","ElvUI Alt-Combat", [[Interface\AddOns\ElvUI\media\fonts\DieDieDie.ttf]])
+LSM:Register("font","ElvUI Pixel 2", [[Interface\AddOns\ElvUI\media\fonts\visitor2.ttf]])
+LSM:Register("sound","ElvUI Warning", [[Interface\AddOns\ElvUI\media\sounds\warning.mp3]])
+LSM:Register("sound","ElvUI Whisper", [[Interface\AddOns\ElvUI\media\sounds\whisper.mp3]])
